883c8ffaf47f70497d23d6cd42b39d02e84b9fb5
siva-ranjani-r <siva2300rs@gmail.com>
2022-12-22 11:10:21 +0530

    Adding js file.

---
+++ js/odd.js
@@ -0,0 +1,23 @@
+/*var num=[8,6,5,7,3,4];
+var odd=[];
+for(number of num)
+{
+	if(number%2==1)
+	{
+		odd.push(number);
+	}
+	else
+	{
+		odd.push(number+"is even");
+	}
+}
+document.write(odd);*/
+
+var num=[8,6,5,7,3,4];
+for(i=0;i<num.length;i++)
+{
+	if(num[i]%2==0)
+	{
+	document.write(num[i]+"<br>");
+	}
+}
